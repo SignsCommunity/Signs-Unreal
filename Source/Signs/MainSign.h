fb4a4b17a192fe3e7d904e55148997773d770691
i354314 <miki.mitevski01@sap.com>
2018-12-03 19:25:21 +0100

    Adds functionality to read and react on input (fire the sign)

--- Source/Signs/MainSign.h
+++ Source/Signs/MainSign.h
@@ -7,6 +7,16 @@
 #include <EngineClasses.h>
 #include "MainSign.generated.h"
 
+/**
+ * Represents the state of the actor
+ */
+UENUM(BlueprintType)
+enum class EStateEnum : uint8 {
+	FIRED       UMETA(DisplayName = "Fired"),
+	ROTATING    UMETA(DisplayName = "Rotating"),
+	IDLE        UMETA(DisplayName = "Idle")
+};
+
 UCLASS()
 class SIGNS_API AMainSign : public APawn
 {
@@ -20,7 +30,57 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	void InitSignCoreAndTrail();
+private:
+    /** Creates and initializes the components of the actor */
+	void InitComponents();
+
+	float RunningTime;
+
+	EStateEnum State;
+
+	FTimerHandle FiredTimerHandle;
+
+	/**
+	 * Calculates the next location of the actor based on the "rotating" logic
+	 * @param DeltaTime - taken from Tick()
+	 */
+	void ContinueOrbitalPath(float DeltaTime);
+
+	UFUNCTION()
+	void ReturnToOrbit();
+
+	/** The time that the sign is in a fired state (in seconds) */
+	UPROPERTY(EditAnywhere, Category = "FiredState")
+	float MaxFiredTime;
+
+	UPROPERTY(EditAnywhere, Category = "OrbitalBehaviour")
+	float OrbitalRadius;
+
+	UPROPERTY(EditAnywhere, Category = "OrbitalBehaviour")
+	float AngularVelocity;
+
+	UPROPERTY(EditAnywhere, Category = "OrbitalBehaviour")
+	float StartAngle;
+
+	UPROPERTY(EditAnywhere, Category = "OrbitalBehaviour")
+	float RelativeHeight;
+
+	UPROPERTY(EditAnywhere, Category = "Appearance")
+	float SignCoreInnerRadius;
+
+	//Components
+	UPROPERTY(EditAnywhere, Category = "Appearance")
+	USphereComponent *SignCoreComponent;
+
+	UPROPERTY(EditAnywhere, Category = "Appearance")
+	UStaticMeshComponent *SignCoreVisual;
+
+	UPROPERTY(EditAnywhere, Category = "Appearance")
+	UParticleSystemComponent *TrailParticleSystem;
+
+	UPROPERTY(VisibleAnywhere, Category = Movement)
+	UProjectileMovementComponent *MovementComponent;
+
 
 public:	
 	// Called every frame
@@ -29,27 +89,9 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	float RunningTime;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitalBehaviour")
-	float OrbitalRadius;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitalBehaviour")
-	float AngularVelocity;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitalBehaviour")
-	float StartAngle;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitalBehaviour")
-	float RelativeHeight;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Appearance")
-	UStaticMeshComponent *SignCoreVisual;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Appearance")
-	UParticleSystemComponent *TrailParticleSystem;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Appearance")
-	float SignCoreInnerRadius;
-
+	/**
+	 * Checks if the actor can be fired and if so it executes the "fire" logic
+	 * @param Direction - The difference between the cursor location and the player (as vector)
+	 */
+	void TryFire(FVector Direction);
 };
