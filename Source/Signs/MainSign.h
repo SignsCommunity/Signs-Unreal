0d4e01556fa56cc028fd6c601607f9681157326b
Miki Mitevski <mitevski1201miki@gmail.com>
2018-12-25 15:59:50 +0100

    Adds basic collision logic. Adds the logic for the returning state.

--- Source/Signs/MainSign.h
+++ Source/Signs/MainSign.h
@@ -14,11 +14,12 @@ UENUM(BlueprintType)
 enum class EStateEnum : uint8 {
 	FIRED       UMETA(DisplayName = "Fired"),
 	ROTATING    UMETA(DisplayName = "Rotating"),
+	RETURNING   UMETA(DisplayName = "Returning"),
 	IDLE        UMETA(DisplayName = "Idle")
 };
 
 UCLASS()
-class SIGNS_API AMainSign : public APawn
+class SIGNS_API AMainSign : public AActor
 {
 	GENERATED_BODY()
 
@@ -34,35 +35,75 @@ private:
     /** Creates and initializes the components of the actor */
 	void InitComponents();
 
+	/** Initializes the default values for the variables */
+	void InitVariables();
+
 	float RunningTime;
+
+	/** It multiples the initial speed. Used for lowering the speed when the sign is getting close to the orbit */
+	float CurrentReturnSpeedRatio;
 
 	EStateEnum State;
 
 	FTimerHandle FiredTimerHandle;
 
 	/**
-	 * Calculates the next location of the actor based on the "rotating" logic
+	 * Calculates the next location of the actor based on the ROTATING state logic
 	 * @param DeltaTime - taken from Tick()
 	 */
 	void ContinueOrbitalPath(float DeltaTime);
 
+	/** Changes the state of the sign */
 	UFUNCTION()
-	void ReturnToOrbit();
+	void ChangeState();
+
+	/**
+	 * Calculates the path when the sign is in a RETURNING state
+	 * @param DeltaTime - taken from Tick()
+	 */
+	UFUNCTION()
+	void UpdateReturnPath(float DeltaTime);
+
+	virtual void NotifyHit(class UPrimitiveComponent* MyComp, AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
 
 	/** The time that the sign is in a fired state (in seconds) */
 	UPROPERTY(EditAnywhere, Category = "FiredState")
 	float MaxFiredTime;
 
-	UPROPERTY(EditAnywhere, Category = "OrbitalBehaviour")
+	/** Multiplies the velocity on impact with other actor */
+	UPROPERTY(EditAnywhere, Category = "FiredState")
+	float ImpulseMultiplier;
+
+	/** The radius when the sign is absorbed in the orbit */
+	UPROPERTY(EditAnywhere, Category = "ReturnState")
+	float PullRadius;
+
+	/** The radius when the speed starts decreasing */
+	UPROPERTY(EditAnywhere, Category = "ReturnState")
+	float DecreaseSpeedRadius;
+
+	/** The starting speed ratio */
+	UPROPERTY(EditAnywhere, Category = "ReturnState")
+	float SpeedRatioTopBoundary;
+
+	/** The limit when the speed stops decreasing */
+	UPROPERTY(EditAnywhere, Category = "ReturnState")
+	float SpeedRatioDownBoundary;
+
+	/** Multiples DeltaTime and subtracts from current speed */
+	UPROPERTY(EditAnywhere, Category = "ReturnState")
+	float DeltaTimeMultiplier;
+
+	UPROPERTY(EditAnywhere, Category = "RotatingState")
 	float OrbitalRadius;
 
-	UPROPERTY(EditAnywhere, Category = "OrbitalBehaviour")
+	UPROPERTY(EditAnywhere, Category = "RotatingState")
 	float AngularVelocity;
 
-	UPROPERTY(EditAnywhere, Category = "OrbitalBehaviour")
+	UPROPERTY(EditAnywhere, Category = "RotatingState")
 	float StartAngle;
 
-	UPROPERTY(EditAnywhere, Category = "OrbitalBehaviour")
+	UPROPERTY(EditAnywhere, Category = "RotatingState")
 	float RelativeHeight;
 
 	UPROPERTY(EditAnywhere, Category = "Appearance")
@@ -78,16 +119,13 @@ private:
 	UPROPERTY(EditAnywhere, Category = "Appearance")
 	UParticleSystemComponent *TrailParticleSystem;
 
-	UPROPERTY(VisibleAnywhere, Category = Movement)
+	UPROPERTY(EditAnywhere, Category = Movement)
 	UProjectileMovementComponent *MovementComponent;
 
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	/**
 	 * Checks if the actor can be fired and if so it executes the "fire" logic
